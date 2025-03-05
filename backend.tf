895368486b00ecd1be913a30f00cf4c5bcaf2dc9
nk837 <nk9617522@gmail.com>
2025-03-05 16:40:02 +0530

    value defined in backend

--- backend.tf
+++ backend.tf
@@ -1,26 +1,26 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "value"
-    storage_account_name = "value"
-    container_name = "value"
-    key = "value"
+    resource_group_name = "demo_rg"
+    storage_account_name = "examplestoracc6837"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
   }
 }
 
 resource "azurerm_resource_group" "demo" {
-  name = "demo-rg"
+  name     = "demo_rg"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "demo" {
-  name = "examplestoracc6837"
-  resource_group_name = azurerm_resource_group.demo.name
-  location = azurerm_resource_group.demo.location
-  account_tier = "standard"
+  name                     = "examplestoracc6837"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "demo" {
-  name = "tfstate"
-  storage_account_name = azurerm_resource_group.demo.name
+  name                 = "tfstate"
+  storage_account_name = azurerm_storage_account.demo.name
 }
