f903ea1bd1809b5b3d02eb47bef7a1f42b72e6d1
nk837 <nk9617522@gmail.com>
2025-03-05 15:45:10 +0530

    try1

--- main.tf
+++ main.tf
@@ -1,25 +1,7 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.21.1"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacct"
-  resource_group_name       = "example-rg"
-  location                 = "East US"
-  account_tier               = "Standard"
+resource "azurerm_storage_account" "demo2" {
+  name = "examplestorageacc6837"
+  resource_group_name = azurerm_resource_group.demo.name
+  location = azurerm_resource_group.demo.location
+  account_tier = "standard"
   account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "example" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.example.name
 }
