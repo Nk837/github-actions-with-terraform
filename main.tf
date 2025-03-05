817b84003d60358f34fa45925f2820c25ff90ae1
nk837 <nk9617522@gmail.com>
2025-03-05 16:47:04 +0530

    backend created

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "azurerm_storage_account" "demo2" {
-  name                     = "examplestorageacc6837"
-  resource_group_name      = azurerm_resource_group.demo.name
-  location                 = azurerm_resource_group.demo.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "demo2" {
+#   name                     = "examplestorageacc6837"
+#   resource_group_name      = azurerm_resource_group.demo.name
+#   location                 = azurerm_resource_group.demo.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
