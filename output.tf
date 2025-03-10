2c3eaeb00cbb48d86f3785002008cece99753101
nk837 <nk9617522@gmail.com>
2025-03-10 18:57:12 +0530

    delete the infra

--- output.tf
+++ output.tf
@@ -1,17 +1,17 @@
-# Output the Static Website URL
-output "static_website_url" {
-  value = "https://${azurerm_storage_account.demo2.primary_web_endpoint}"
-}
+# # Output the Static Website URL
+# output "static_website_url" {
+#   value = "https://${azurerm_storage_account.demo2.primary_web_endpoint}"
+# }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.demo2.name
-}
+# output "storage_account_name" {
+#   value = azurerm_storage_account.demo2.name
+# }
 
-output "storage_account_key" {
-  value = azurerm_storage_account.demo2.primary_access_key
-  sensitive = true
-}
+# output "storage_account_key" {
+#   value = azurerm_storage_account.demo2.primary_access_key
+#   sensitive = true
+# }
 
-output "storage_container_name" {
-  value = azurerm_storage_container.demo2.name
-}
+# output "storage_container_name" {
+#   value = azurerm_storage_container.demo2.name
+# }
